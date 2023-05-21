5f7f954ca5673c4b73cfd0f5a474864a0778c716
Jonathanpv99 <jonyvegapjv@gmail.com>
2023-05-20 23:18:48 -0600

    inicio registro materias

--- WikiDocente.py
+++ WikiDocente.py
@@ -2,12 +2,51 @@ import sys
 from tkinter import *
 import tkinter as tk
 import PIL
+import mysql.connector
 from PIL import Image, ImageTk
 from tkinter import ttk, scrolledtext
 
 
 def cerrar_proyecto():
     sys.exit()
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    )
+
+mycursor = mydb.cursor()
+mycursor.execute("USE tareatopicos")
+
+def buscar():
+    semestre = cmbSemestre.get()
+    mycursor.execute("SELECT nombreMat FROM materias where semestre='"+semestre+"';")
+    rows = mycursor.fetchall()
+
+    # Borrar los elementos del ComboBox
+    cmbMateria['values'] = ()
+    #llenar cmb
+    for row in rows:
+        cmbMateria['values'] = (*cmbMateria['values'], row[0])
+
+
+def buscarId():
+    materia = cmbMateria.get()
+    mycursor.execute("select id from materias where nombreMat='" + materia + "';")
+    resultado = mycursor.fetchone()
+    id = resultado[0]
+    print(id)
+
+def registrar(id):
+    unidad = cmbUnidad.get()
+    tema = tema_entry.get()
+    info = info_area.get("1.0", tk.END)
+    sqlComando = "INSERT INTO informacion (unidad, tema, info, materiaId) VALUES (%s, %s, %s, %s)"
+    record1 = (unidad, tema, info, id)
+    mycursor.execute(sqlComando, record1)
+    mydb.commit()
+    welcome_label.config(text="¡Registrado con exito " + "!")
 
 def login():
     window.withdraw()
@@ -50,7 +89,7 @@ semestre_label.place(x=305, y=100)
 cmbSemestre = ttk.Combobox(window,values=["1", "2", "3", "4", "5", "6", "7", "8"])
 cmbSemestre.place(x=395, y=100)
 # Crear botón de busqueda
-busc_button = Button(window, text="Buscar materias", font=("Arial", 11), bg="#052E1A", fg="#ffffff")
+busc_button = Button(window, text="Buscar materias", font=("Arial", 11), bg="#052E1A", fg="#ffffff", command=buscar)
 busc_button.place(x=570, y=90)
 
 pass_label = Label(window, text="Materia:", font=("Arial", 12), bg="#006633", fg="#ffffff")
@@ -64,7 +103,7 @@ cmbUnidad= ttk.Combobox(window,values=["1", "2", "3", "4", "5", "6", "7", "8", "
 cmbUnidad.place(x=110, y=200)
 tema_label = Label(window, text="Tema:", font=("Arial", 12), bg="#006633", fg="#ffffff")
 tema_label.place(x=30, y=250)
-tema_entry = Entry(window, font=("Arial", 12), show="*")
+tema_entry = Entry(window, font=("Arial", 12))
 tema_entry.place(x=110, y=250)
 info_label = Label(window, text="Informacion:", font=("Arial", 12), bg="#006633", fg="#ffffff")
 info_label.place(x=350, y=150)
@@ -72,13 +111,13 @@ info_area = scrolledtext.ScrolledText(window, width=40, height=10, font=("Arial"
 info_area.place(x=350, y=180)
 
 # Crear botón de Registro
-login_button = Button(window, text="Registrar", font=("Arial", 12), bg="#052E1A", fg="#ffffff", command=login)
+login_button = Button(window, text="Registrar", font=("Arial", 12), bg="#052E1A", fg="#ffffff", command=buscarId())
 login_button.place(x=620, y=380)
 
 
-# Crear etiqueta de mensaje de bienvenida
+# Crear etiqueta de mensaje de registro
 welcome_label = Label(window, text="", font=("Arial", 12), bg="#006633", fg="#ffffff")
-welcome_label.place(x=50, y=500)
+welcome_label.place(x=400, y=385)
 
 # panel inferior ------------------------------------------------------------
 panel_inferior = Frame(window, bd=1, relief=FLAT)
